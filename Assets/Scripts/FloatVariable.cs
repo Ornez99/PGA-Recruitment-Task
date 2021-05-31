3cd59faedc4faaa5ce075396496ac1089a4e59e7
Ornez99 <lukasz.chudy99@gmail.com>
2021-05-31 18:00:12 +0200

    Added Zenject Created UI Added scriptable object data for best time and current time

---
+++ Assets/Scripts/FloatVariable.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Float", menuName = "ScriptableObjects/Float variable", order = 1)]
+public class FloatVariable : ScriptableObject
+{
+    [SerializeField]
+    private float value;
+
+    public float Value { get => value; set => this.value = value; }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rigidbody;
+
+    //private 
+}
